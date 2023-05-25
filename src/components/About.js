d3477dee956d453b8eb8c4b5fb3d8999dde84a52
unknown <joseroniz18@gmail.com>
2023-05-25 13:31:28 -0700

    trakie los demas archivos ahora que ya pude hacer el deploy en netliy

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hola👋 soy Jose Luis Arteaga,
+            <br className="hidden lg:inline-block" /> Me encanta construir aplicaciones web.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Me desempeño como Front end developer, especialmente con tecnologias como JavaScript y React JS,
+            ademas de tener nociones en UX/UI Design entre otras certificaciones relacionadas a la 
+            tecnologia.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-4">
+              Trabaja conmigo
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Mira mis trabajos anteriores
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img src='./profile.png' alt="Perfil" width="300px"/>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,115 @@
+export const projects = [
+  {
+    title: "Calculadora 3D",
+    subtitle: "JavaScript",
+    description:
+      "Esta calculadora la realice con la intencion de hacerla esteticamente atractiva con ayuda de html, css y JavaScript ademas probando cosas nuevas",
+    image: "./project1.png",
+    link: "https://harmonious-concha-03489b.netlify.app/",
+  },
+  {
+    title: "Carrito inteligente",
+    subtitle: "JavaScript",
+    description:
+      "Este proyecto lo trabaje con html, css, bootstrap y JavaScript haciendo uso del local storage para que los productos pudieran guardarse aun refrescando la pagina despues de agregar productos al carrito",
+    image: "./project2.png",
+    link: "https://wondrous-alpaca-68ae74.netlify.app/",
+  },
+  {
+    title: "Checkout Dinamico",
+    subtitle: "JavaScript",
+    description:
+      "El checkout dinamico que cree, es muy interesante por que detecta el tipo de tarjeta que se este escribiendo, tambien gira de forma 3D la tarjeta sin duda alguna un proyecto muy bonito que me encanto hacer",
+    image: "./project3.png",
+    link: "https://capable-pie-e5e75b.netlify.app/",
+  },
+  {
+    title: "Validador de identidad",
+    subtitle: "JavaScript",
+    description:
+      "Este proyecto es algo basico pero sirve para validar la identidad de un usuario consultando atravez de un objeto en JavaScript necesitas los siguientes datos para probarlo, dni:5555 date: 2023-02-09",
+    image: "./project4.png",
+    link: "https://sensational-crepe-e094a4.netlify.app/",
+  },
+  {
+    title: "Twitter CLON UX / UI",
+    subtitle: "HTML, CSS3, SASS, BOOTSTRAP",
+    description:
+      "Este proyecto lo hice gracias a una plantilla de FIGMA busque un diseno mas moderno similar a twitter para practicar mis habilidades maquetando con BOOTSTRAP",
+    image: "./project5.png",
+    link: "https://thunderous-buttercream-e01a4a.netlify.app/",
+  },
+  {
+    title: "Dragon ball Website",
+    subtitle: "JavaScript",
+    description:
+      "Este es un proyecto cuando recien comenzaba a aprender JavaScript con el uso de librerias para aprender a hacer efectos, en esta web practique el scroll Trigger y Parallax",
+    image: "./project6.png",
+    link: "https://646ea0537be7e73c6af2f55f--tangerine-monstera-0dc18c.netlify.app/",
+  },
+  {
+    title: "Clon mercado libre",
+    subtitle: "HTML, CSS3",
+    description:
+      "Este es un proyecto basico en el cual el objetivo fue practicar con Flexbox para el correcto posicionamiento de las cajas me inspire en mercadolibre creo que el resultado fue bueno",
+    image: "./project7.png",
+    link: "https://646ed6a78bc2084913556c9a--classy-churros-c1585a.netlify.app/",
+  },
+  {
+    title: "Mi portafolio personal",
+    subtitle: "React JS",
+    description:
+      "Mis conocimientos en React son basicos, pero me motive a empezar a exponerme mas a este entorno que no conozco mucho para empezar a sentirme cada vez mas comodo programando en React",
+    image: "./project8.png",
+    link: "#",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Aunque mi Licenciatura en Ingeniería en Sistemas Computacionales quedó trunca después de un semestre, eso no me ha detenido en mi pasión por la programación. He continuado mi formación a través de cursos en línea y tutoriales en YouTube. Me apasiona la programación y estoy decidido a seguir aprendiendo y creciendo en este campo. A pesar de los obstáculos, mi determinación sigue intacta y estoy emocionado por lo que el futuro me depara en el mundo de la tecnología.",
+    image: "./uveg_logo.jpg",
+    name: "Lic. en Ingenieria en sistemas (trunca)",
+    company: "UVEG Universidad",
+    link2: "#"
+  },
+  {
+    quote:
+      "He completado con éxito el Master en CSS, especializado en Responsive Design, SASS, Flexbox, Grid y Bootstrap. Mi dominio completo de estos temas me permite crear sitios web altamente funcionales y visualmente atractivos. Me siento seguro en mi capacidad para implementar diseños adaptables y estructuras de página eficientes. Estoy comprometido en mantenerme actualizado en las mejores prácticas de CSS y en seguir explorando nuevas técnicas para mejorar aún más mis habilidades.",
+    image: "./Udemy-logo.png",
+    name: "Master en CSS: Responsive, SASS, Flexbox, Grid y Bootstrap",
+    company: "Plataforma de cursos online Udemy",
+    link2:"https://www.udemy.com/certificate/UC-74e0e457-46ca-4bd9-8569-681c3ce8fe2c/"
+  },
+
+  {
+    quote:
+      "Terminé el curso de Programador en JavaScript y estoy emocionado con lo que he aprendido. Ahora tengo conocimientos intermedios en el lenguaje y puedo crear interacciones básicas en sitios web. Me siento confiado en mi capacidad para resolver problemas y seguir aprendiendo en esta área. ¡El mundo del JavaScript me tiene entusiasmado!",
+    image: "./edutin_logo.jpg",
+    name: "Programador JavaScript",
+    company: "Plataform de cursos Edutin",
+    link2: "https://app.edutin.com/verify/6948111",
+  },
+
+  {
+    quote:
+      "Decidí inscribirme en el bootcamp de Kodigo Academy para reforzar mis conocimientos en JavaScript y explorar conceptos más avanzados. Aunque aún no he terminado el programa, ya he mejorado significativamente mi comprensión de JavaScript. En cuanto a React, solo tengo conocimientos básicos, pero estoy emocionado por adentrarme en este poderoso framework y ampliar mis habilidades de desarrollo web. ¡Estoy comprometido en aprender y crecer en mi carrera como programador!",
+    image: "./kodigo-logo.jpg",
+    name: "Web developer (en curso)",
+    company: "Academia Kodigo",
+    link2: "#",
+  },
+  
+];
+
+export const skills = [
+  "JavaScript",
+  "React JS",
+  "HTML & CSS3",
+  "SASS",
+  "BOOTSTRAP & Tailwind",
+  "FIGMA",
+  "WordPress & Elementor",
+  "Git & GitHub",
+];
