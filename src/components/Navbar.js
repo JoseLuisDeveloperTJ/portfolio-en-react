dfb820f24531f1a95b9caec2bf2b751d1f50fdfc
unknown <joseroniz18@gmail.com>
2023-08-18 15:00:53 -0700

    agregue un boton para descargar mi cv

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,9 +23,9 @@ export default function Navbar() {
           </a>
         </nav>
         <a
-          href="#contact"
+          href="./JoseLuis_Arteaga_WebDeveloper5.pdf" download
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          ¡Contrátame!
+          ¡Descarga mi CV!
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
